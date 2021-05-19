c6f307da140f89893c81e8e9f9dc4adfd5641b82
Diego Edmilson Pamo Castro <diego.pamo@tecsup.edu.pe>
2021-05-19 13:04:07 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Diego Edmilson on 5/19/21.
+//  Copyright © 2021 Diego Edmilson Pamo Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
